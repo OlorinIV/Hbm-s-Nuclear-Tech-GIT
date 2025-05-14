00a18dcd2e5a7544d6ed7f6595e2ec81e30adffc
OlorinV <void_traveler@foxmail.com>
2025-05-14 20:12:45 +0800

    fix lemegeton irradiation

--- src/main/java/com/hbm/tileentity/machine/rbmk/TileEntityRBMKOutgasser.java
+++ src/main/java/com/hbm/tileentity/machine/rbmk/TileEntityRBMKOutgasser.java
@@ -36,6 +36,7 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 	public FluidTank gas;
 	public double progress;
 	public long duration = 10000L;
+	public boolean idct = false;
 
 	public TileEntityRBMKOutgasser() {
 		super(2);
@@ -53,7 +54,7 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 		if(!worldObj.isRemote) {
 			if(slots[0] != null){
 				Triplet<ItemStack, FluidStack, Long> output = OutgasserRecipes.getOutput(slots[0]);
-				if(output != null) this.duration = (long)output.getZ();
+				if(output != null) this.duration = output.getZ();
 			} else {
 				this.duration = 10000L;
 				this.progress = 0;
@@ -66,6 +67,8 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 			for(DirPos pos : getOutputPos()) {
 				if(this.gas.getFill() > 0) this.sendFluid(gas, worldObj, pos.getX(), pos.getY(), pos.getZ(), pos.getDir());
 			}
+
+			this.idct = false;
 		}
 
 		super.updateEntity();
@@ -101,6 +104,17 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 
 	@Override
 	public void receiveFlux(NeutronStream stream) {
+
+		if(!this.idct) {
+			if(slots[0] != null){
+				Triplet<ItemStack, FluidStack, Long> output = OutgasserRecipes.getOutput(slots[0]);
+				if(output != null) this.duration = output.getZ();
+			} else {
+				this.duration = 10000L;
+				this.progress = 0;
+			}
+			this.idct = true;
+		}
 
 		if(canProcess()) {
 
@@ -218,6 +232,7 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 		super.serialize(buf);
 		this.gas.serialize(buf);
 		buf.writeDouble(this.progress);
+		buf.writeLong(this.duration);
 	}
 
 	@Override
@@ -225,6 +240,7 @@ public class TileEntityRBMKOutgasser extends TileEntityRBMKSlottedBase implement
 		super.deserialize(buf);
 		this.gas.deserialize(buf);
 		this.progress = buf.readDouble();
+		this.duration = buf.readLong();
 	}
 
 	@Override
