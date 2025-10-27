c7c5a15038aafea8ed022d09654b69ba4391025b
OlorinV <void_traveler@foxmail.com>
2025-10-27 16:45:49 +0800

    final touches

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,20 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+val mod_build_number: String by project
+val mod_build_number_sub: String by project
+val credits: String by project
+
+val version = "X$mod_build_number.$mod_build_number_sub"
+
+tasks.processResources.configure {
+    
+    filesMatching("mcmod.info") {
+        expand(mapOf("version" to version, "credits" to credits))
+    }
+}
+
+tasks.jar.configure {
+    archiveFileName = "NTMC_$version.jar"
+}
